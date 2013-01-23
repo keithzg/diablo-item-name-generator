c6006bb34c315aaacf151ec6a2654bcd4323c37b
keithzg <keithzg@gmail.com>
2013-01-22 23:43:46 -0700

    replaced nouns with list file

--- names.py
+++ names.py
@@ -2,12 +2,13 @@
 
 import random
 
-adjective = ['magnificent', 'evil', 'cynical', 'broken', 'irritating', 'hearty', 'infernal', 'fluctuating', 'sacred', 'doomed', 'miserly', 'leeching', 'angelic', 'tumultuous', 'woeful', 'despairing', 'merciful', 'frail', 'flaming', 'pious', 'budget', 'crustaceous', 'brusque', 'choleric', 'irascible', 'saturnine', 'gruff', 'impetuous', 'feline', 'blighted', 'mischevious', 'malignant', 'gay', 'gainly', 'gangrenous', 'baleful', 'vicious', 'cursed', 'virginal', 'austere', 'defiled', 'terrible', 'cruel']
-noun = [ 'eye', 'helm', 'rod', 'bell', 'polearm', 'axe', 'bow', 'quiver', 'staff', 'blade', 'targe', 'scythe', 'halberd', 'dagger', 'knife', 'great axe', 'gloves', 'ring', 'wand', 'breastplate']
-purpose = ['splendor', 'magnificence', 'keening', 'reaming', 'the monkey', 'spiritual awakening', 'frugality', 'the bear', 'the bat', 'the rhino', 'laceration', 'woe', 'despair', 'allotment', 'mercy', 'verisimilitude', 'piety', 'bludgeoning', 'gangrene', 'bane', 'purity']
+adjective = ['magnificent', 'evil', 'cynical', 'broken', 'irritating', 'hearty', 'infernal', 'fluctuating', 'sacred', 'doomed', 'miserly', 'leeching', 'angelic', 'tumultuous', 'woeful', 'despairing', 'merciful', 'frail', 'flaming', 'pious', 'budget', 'crustaceous', 'brusque', 'choleric', 'irascible', 'saturnine', 'gruff', 'impetuous', 'feline', 'blighted', 'mischevious', 'malignant', 'gay', 'gainly', 'gangrenous', 'baleful', 'vicious', 'cursed', 'virginal', 'austere', 'defiled', 'terrible', 'cruel', 'lazy']
+#noun = [ 'eye', 'helm', 'rod', 'bell', 'polearm', 'axe', 'bow', 'quiver', 'staff', 'blade', 'targe', 'scythe', 'halberd', 'dagger', 'knife', 'great axe', 'gloves', 'ring', 'wand', 'breastplate']
+nouns = [line.strip() for line in open('nouns')]
+purpose = ['splendor', 'magnificence', 'keening', 'reaming', 'the monkey', 'spiritual awakening', 'frugality', 'the bear', 'the bat', 'the rhino', 'laceration', 'woe', 'despair', 'allotment', 'mercy', 'verisimilitude', 'piety', 'bludgeoning', 'gangrene', 'bane', 'purity', 'the possum']
 
 first = random.choice(adjective)
-second = random.choice(noun)
+second = random.choice(nouns)
 third = random.choice(purpose)
 
 print first + " " + second + " of " + third
