8ab028abdbff2e2808ee539030f571b523f48df9
keithzg <keithzg@gmail.com>
2013-01-22 23:51:15 -0700

    move wordlists out into discrete files

--- names.py
+++ names.py
@@ -2,14 +2,13 @@
 
 import random
 
-adjective = ['magnificent', 'evil', 'cynical', 'broken', 'irritating', 'hearty', 'infernal', 'fluctuating', 'sacred', 'doomed', 'miserly', 'leeching', 'angelic', 'tumultuous', 'woeful', 'despairing', 'merciful', 'frail', 'flaming', 'pious', 'budget', 'crustaceous', 'brusque', 'choleric', 'irascible', 'saturnine', 'gruff', 'impetuous', 'feline', 'blighted', 'mischevious', 'malignant', 'gay', 'gainly', 'gangrenous', 'baleful', 'vicious', 'cursed', 'virginal', 'austere', 'defiled', 'terrible', 'cruel', 'lazy']
-#noun = [ 'eye', 'helm', 'rod', 'bell', 'polearm', 'axe', 'bow', 'quiver', 'staff', 'blade', 'targe', 'scythe', 'halberd', 'dagger', 'knife', 'great axe', 'gloves', 'ring', 'wand', 'breastplate']
+adjectives =  [line.strip() for line in open('adjectives')]
 nouns = [line.strip() for line in open('nouns')]
-purpose = ['splendor', 'magnificence', 'keening', 'reaming', 'the monkey', 'spiritual awakening', 'frugality', 'the bear', 'the bat', 'the rhino', 'laceration', 'woe', 'despair', 'allotment', 'mercy', 'verisimilitude', 'piety', 'bludgeoning', 'gangrene', 'bane', 'purity', 'the possum']
+purposes = [line.strip() for line in open('purposes')]
 
-first = random.choice(adjective)
+first = random.choice(adjectives)
 second = random.choice(nouns)
-third = random.choice(purpose)
+third = random.choice(purposes)
 
 print first + " " + second + " of " + third
 
