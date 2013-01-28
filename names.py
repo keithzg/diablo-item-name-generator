e4d7dbb8dac26599c90e69ed7a46a7080c81ae5b
keithzg <keithzg@gmail.com>
2013-01-27 23:39:19 -0700

    Renamed 'purposes' to 'suffixes' (but, should it be 'suffices"? naw, that still looks wrong)

--- names.py
+++ names.py
@@ -4,11 +4,11 @@ import random
 
 adjectives =  [line.strip() for line in open('adjectives')]
 nouns = [line.strip() for line in open('nouns')]
-purposes = [line.strip() for line in open('purposes')]
+suffixes = [line.strip() for line in open('suffixes')]
 
 first = random.choice(adjectives)
 second = random.choice(nouns)
-third = random.choice(purposes)
+third = random.choice(suffixes)
 
 print first + " " + second + " of " + third
 
